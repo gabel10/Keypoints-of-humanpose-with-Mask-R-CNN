750ce0519463ebd3f5f47a14a5951723bdb17ad7
Abel Gabriel Zapata <zapata.gabriel.abel@gmail.com>
2021-06-11 22:03:28 -0500

    update detection of bodyweight

--- model.py
+++ model.py
@@ -961,6 +961,120 @@ def refine_detections_graph(rois, probs, deltas, window, config):
     detections = tf.reshape(detections,[config.DETECTION_MAX_INSTANCES,6])
     return detections
 
+def refine_detections_bw_graph(rois, probs, deltas, window, bodyweights,config):
+    """Refine classified proposals and filter overlaps and return final
+    detections.
+
+    Inputs:
+        rois: [N, (y1, x1, y2, x2)] in normalized coordinates
+        probs: [N, num_classes]. Class probabilities.
+        deltas: [N, num_classes, (dy, dx, log(dh), log(dw))]. Class-specific
+                bounding box deltas.
+        window: (y1, x1, y2, x2) in image coordinates. The part of the image
+            that contains the image excluding the padding.
+        bodyweights: [N, num_classes]. Bodyweights
+
+    Returns detections : [N, (y1, x1, y2, x2, class_id, score, bodyweight)] where
+            coordinates are in image domain.
+            keypoint_weights:[N, num_keypoints]
+    """
+    # Class IDs per ROI
+    class_ids = tf.argmax(probs, axis=1, output_type=tf.int32) # return the index
+    # Class probability of the top class of each ROI
+    indices = tf.stack([tf.range(probs.shape[0]), class_ids], axis=1)
+    class_scores = tf.gather_nd(probs, indices)
+    # Class-specific bounding box deltas
+    deltas_specific = tf.gather_nd(deltas, indices)
+    bodyweight_scores = tf.gather_nd(bodyweights, indices)*tf.constant([4000.0])
+
+    # Apply bounding box deltas
+    # Shape: [boxes, (y1, x1, y2, x2)] in normalized coordinates
+    refined_rois = apply_box_deltas_graph(
+        rois, deltas_specific * config.BBOX_STD_DEV)
+    # Convert coordiates to image domain
+    # TODO: better to keep them normalized until later
+    height, width = config.IMAGE_SHAPE[:2]
+    refined_rois *= tf.constant([height, width, height, width], dtype=tf.float32)
+    # Clip boxes to image window
+    refined_rois = clip_boxes_graph(refined_rois, window)
+    # Round and cast to int since we're deadling with pixels now
+    refined_rois = tf.to_int32(tf.rint(refined_rois))
+
+    # TODO: Filter out boxes with zero area
+
+    # Filter out background boxes
+    keep = tf.where(class_ids > 0)[:, 0]
+    # Filter out low confidence boxes
+    if config.DETECTION_MIN_CONFIDENCE:
+        conf_keep = tf.where(class_scores >= config.DETECTION_MIN_CONFIDENCE)[:, 0]
+        keep = tf.sets.set_intersection(tf.expand_dims(keep, 0),
+                                        tf.expand_dims(conf_keep, 0))
+        keep = tf.sparse_tensor_to_dense(keep)[0]
+
+    # Apply per-class NMS
+    # 1. Prepare variables
+    pre_nms_class_ids = tf.gather(class_ids, keep)
+    pre_nms_scores = tf.gather(class_scores, keep)
+    pre_nms_rois = tf.gather(refined_rois,   keep)
+    pre_nms_bodyweight = tf.gather(bodyweight_scores, keep)
+    # pre_nms_keypoint_weights = tf.gather(keypoint_weights, keep)
+    unique_pre_nms_class_ids = tf.unique(pre_nms_class_ids)[0]
+
+    def nms_keep_map(class_id):
+        """Apply Non-Maximum Suppression on ROIs of the given class."""
+        # Indices of ROIs of the given class
+        ixs = tf.where(tf.equal(pre_nms_class_ids, class_id))[:, 0]
+        # Apply NMS
+        class_keep = tf.image.non_max_suppression(
+                tf.to_float(tf.gather(pre_nms_rois, ixs)),
+                tf.gather(pre_nms_scores, ixs),
+                max_output_size=config.DETECTION_MAX_INSTANCES,
+                iou_threshold=config.DETECTION_NMS_THRESHOLD)
+        # Map indicies
+        class_keep = tf.gather(keep, tf.gather(ixs, class_keep))
+        # Pad with -1 so returned tensors have the same shape
+        gap = config.DETECTION_MAX_INSTANCES - tf.shape(class_keep)[0]
+        class_keep = tf.pad(class_keep, [(0, gap)],
+                            mode='CONSTANT', constant_values=-1)
+        # Set shape so map_fn() can infer result shape
+        class_keep.set_shape([config.DETECTION_MAX_INSTANCES])
+        return class_keep
+
+    # 2. Map over class IDs
+    nms_keep = tf.map_fn(nms_keep_map, unique_pre_nms_class_ids,
+                         dtype=tf.int64)
+    # 3. Merge results into one list, and remove -1 padding
+    nms_keep = tf.reshape(nms_keep, [-1])
+    nms_keep = tf.gather(nms_keep, tf.where(nms_keep > -1)[:, 0])
+    # 4. Compute intersection between keep and nms_keep
+    keep = tf.sets.set_intersection(tf.expand_dims(keep, 0),
+                                    tf.expand_dims(nms_keep, 0))
+    keep = tf.sparse_tensor_to_dense(keep)[0]
+    # Keep top detections
+    roi_count = config.DETECTION_MAX_INSTANCES
+    class_scores_keep = tf.gather(class_scores, keep)
+    num_keep = tf.minimum(tf.shape(class_scores_keep)[0], roi_count)
+    top_ids = tf.nn.top_k(class_scores_keep, k=num_keep, sorted=True)[1]
+    keep = tf.gather(keep, top_ids)
+
+    # Arrange output as [N, (y1, x1, y2, x2, class_id, score)]
+    # Coordinates are in image domain.
+    detections = tf.concat([
+        tf.to_float(tf.gather(refined_rois, keep)),
+        tf.to_float(tf.gather(class_ids, keep))[..., tf.newaxis],
+        tf.gather(class_scores, keep)[..., tf.newaxis],
+        tf.gather(bodyweight_scores, keep)[..., tf.newaxis]
+        ], axis=1)
+
+
+    # Pad with zeros if detections < DETECTION_MAX_INSTANCES
+    gap = config.DETECTION_MAX_INSTANCES - tf.shape(detections)[0]
+    detections = tf.pad(detections, [(0, gap), (0, 0)], "CONSTANT")
+
+    detections = tf.reshape(detections,[config.DETECTION_MAX_INSTANCES,7])
+    return detections
+
+
 
 class DetectionLayer(KE.Layer):
     """Takes classified proposal boxes and their bounding box deltas and
@@ -983,12 +1097,13 @@ class DetectionLayer(KE.Layer):
         mrcnn_class = inputs[1]
         mrcnn_bbox = inputs[2]
         image_meta = inputs[3]
+        mrcnn_bw = inputs[4]
 
         # Run detection refinement graph on each item in the batch
         _, _, window, _ = parse_image_meta_graph(image_meta)
         outputs = utils.batch_slice(
-            [rois, mrcnn_class, mrcnn_bbox, window],
-            lambda x, y, w, z: refine_detections_graph(x, y,  w, z, self.config),
+            [rois, mrcnn_class, mrcnn_bbox, window, mrcnn_bw],
+            lambda x, y, w, z, b: refine_detections_bw_graph(x, y,  w, z, b, self.config),
             self.config.IMAGES_PER_GPU)
 
         # Reshape output
@@ -2692,7 +2807,7 @@ class MaskRCNN():
             #   detections: [batch, num_detections, (y1, x1, y2, x2, class_id, score)] in image coordinates
             #   keypoint_weights: [batch, num_detections, num_keypoints]
             detections = DetectionLayer(config, name="mrcnn_detection")(
-                [rpn_rois, mrcnn_class, mrcnn_bbox,input_image_meta])
+                [rpn_rois, mrcnn_class, mrcnn_bbox,input_image_meta, mrcnn_bodyweight])
 
             # Convert boxes to normalized coordinates
             # TODO: let DetectionLayer return normalized coordinates to avoid
@@ -2712,7 +2827,7 @@ class MaskRCNN():
             """                                                config.NUM_KEYPOINTS) """
 
             model = KM.Model([input_image, input_image_meta],
-                             [detections, mrcnn_class, mrcnn_bbox, rpn_rois, rpn_class, rpn_bbox, mrcnn_mask, mrcnn_bodyweight],
+                             [detections, mrcnn_class, mrcnn_bbox, rpn_rois, rpn_class, rpn_bbox, mrcnn_mask],
                              name='bodyweight_mask_rcnn')
 
         # Add multi-GPU support.
@@ -3086,7 +3201,7 @@ class MaskRCNN():
 
         return boxes, class_ids, scores, full_masks
 
-    def unmold_keypoint_detections(self, detections, mrcnn_bodyweight, image_shape, window, mrcnn_mask):
+    def unmold_keypoint_detections(self, detections, image_shape, window, mrcnn_mask):
         """Reformats the detections of one image from the format of the neural
         network output to a format suitable for use in the rest of the
         application.
@@ -3114,7 +3229,7 @@ class MaskRCNN():
         class_ids = detections[:N, 4].astype(np.int32)
         scores = detections[:N, 5]
         masks = mrcnn_mask[np.arange(N), :, :, class_ids]
-        bodyweights = mrcnn_bodyweight[:N]
+        bodyweights = detections[:N, 6]
 
         # Compute scale and shift to translate coordinates to image domain.
         h_scale = image_shape[0] / (window[2] - window[0])
@@ -3134,7 +3249,7 @@ class MaskRCNN():
         # stages of training when the network weights are still a bit random.
         exclude_ix = np.where(
             (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1]) <= 0)[0]
-        bodyweights = np.multiply(bodyweights, 4000)
+        #bodyweights = np.multiply(bodyweights, 4000)
         if exclude_ix.shape[0] > 0:
             boxes = np.delete(boxes, exclude_ix, axis=0)
             class_ids = np.delete(class_ids, exclude_ix, axis=0)
@@ -3227,7 +3342,7 @@ class MaskRCNN():
         # Run human pose detection
         #[detections, mrcnn_class, mrcnn_bbox, rpn_rois, rpn_class, rpn_bbox, mrcnn_mask, keypoint_mcrcnn_prob]
         detections, mrcnn_class, mrcnn_bbox, \
-            rois, rpn_class, rpn_bbox, mrcnn_mask, mrcnn_bodyweight =\
+            rois, rpn_class, rpn_bbox, mrcnn_mask =\
             self.keras_model.predict([molded_images, image_metas], verbose=0)
         if verbose:
             log("rpn_class", rpn_class)
@@ -3241,13 +3356,11 @@ class MaskRCNN():
             # 100
             log("detections", detections)
             log("mrcnn_mask", mrcnn_mask)
-            log("mrcnn_bodyweight", mrcnn_bodyweight)
         # Process detections
         results = []
         for i, image in enumerate(images):
-            final_rois, final_class_ids, final_scores, final_bodyweight,final_masks=\
-                self.unmold_keypoint_detections(detections[i], mrcnn_bodyweight[i],
-                                       image.shape, windows[i],mrcnn_mask[i])
+            final_rois, final_class_ids, final_scores, final_bodyweight, final_masks=\
+                self.unmold_keypoint_detections(detections[i], image.shape, windows[i], mrcnn_mask[i])
             results.append({
                 "rois": final_rois,
                 "class_ids": final_class_ids,
